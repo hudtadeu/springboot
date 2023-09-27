996409fd44391b900dedc5e106e24e8d254871f5
Hudson Tadeu <hudinvesting@gmail.com>
2023-09-27 09:37:52 -0300

    Criação model

--- src/main/java/br/com/springboot/model/User.java
+++ src/main/java/br/com/springboot/model/User.java
@@ -1,8 +1,17 @@
 package br.com.springboot.model;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+
+  @Getter
+  @Setter
+  @AllArgsConstructor
 public class User {
-  
+
   private Long id;
   private String name;
   private String username;
+
+
 }
