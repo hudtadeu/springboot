4555b84c91ab3e9c1826b9236c79b2b7ec12b9e1
Hudson Tadeu <hudinvesting@gmail.com>
2023-10-02 16:11:08 -0300

    Get mapping

--- src/main/java/br/com/springboot/controllers/UserController.java
+++ src/main/java/br/com/springboot/controllers/UserController.java
@@ -1,10 +1,26 @@
 package br.com.springboot.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.springboot.model.User;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
+  @GetMapping("/")
+  public User user() {
+    User user = new User();
+    user.setId(1L);
+    user.setName("Hudson");
+    user.setUsername("hudsontadeu");
+    return user;
+  }
+
+  @PostMapping("/")
+  public User user() {
+    
+  }
 }
