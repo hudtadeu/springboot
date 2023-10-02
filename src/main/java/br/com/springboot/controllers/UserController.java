f609fbebe7bd84c33f3768da211729ab84bbf68d
Hudson Tadeu <hudinvesting@gmail.com>
2023-10-02 16:50:28 -0300

    Utilizando stream

--- src/main/java/br/com/springboot/controllers/UserController.java
+++ src/main/java/br/com/springboot/controllers/UserController.java
@@ -2,8 +2,10 @@ package br.com.springboot.controllers;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,18 +19,25 @@ public class UserController {
 
   private List<User> users = new ArrayList<>();
 
-  @GetMapping("/")
-  public User user() {
-    User user = new User();
-    user.setId(1L);
-    user.setName("Hudson");
-    user.setUsername("hudsontadeu");
-    return user;
+  @GetMapping("/{id}")
+  public User user(@PathVariable("id") Long id) {
+    Optional <User> userFind = users.stream().filter(user -> user.getId() = id).findFirst();
+
+    if(userFind.isPresent()) {
+      return userFind.get();
+    }
+    
+    return null;
   }
 
   @PostMapping("/")
   public User user(@RequestBody User user) {
     users.add(user);
     return user;
+  }
+
+  @GetMapping("/list")
+  public List<User> list() {
+    return users;
   }
 }
