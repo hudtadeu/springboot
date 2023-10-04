80487834fc7283d6c229d5082a074f63db6433bc
Hudson Tadeu <hudinvesting@gmail.com>
2023-10-04 10:24:04 -0300

    Aplicação interagindo com banco de dados

--- src/main/java/br/com/springboot/model/User.java
+++ src/main/java/br/com/springboot/model/User.java
@@ -2,6 +2,11 @@ package br.com.springboot.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
@@ -9,8 +14,12 @@ import lombok.NoArgsConstructor;
   @Setter
   @AllArgsConstructor
   @NoArgsConstructor
+  @Entity
+  @Table(name = "users")
 public class User {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
   private String name;
   private String username;
